2c146aae3ab1a94c29ff7b99564908ceef78abb9
Sergey Cherkesov <s.cherkesov@pbt.com.mt>
2019-02-15 12:46:22 +0300

    Fix bootstrap for win

--- scripts/bootstrap.bat
+++ scripts/bootstrap.bat
@@ -1,11 +1,11 @@
 @ECHO OFF
 SETLOCAL
 
-mkdir   .\vendor
-cd      .\vendor
+if NOT EXIST ".\vendor" mkdir .\vendor
+cd .\vendor
 
 if NOT EXIST ".\htmlcxx" (
-    (New-Object Net.WebClient).DownloadFile('https://netix.dl.sourceforge.net/project/htmlcxx/htmlcxx/0.86/htmlcxx-0.86.tar.gz', 'htmlcxx-0.86.tar.gz')
+    powershell -Command "Invoke-WebRequest https://netix.dl.sourceforge.net/project/htmlcxx/htmlcxx/0.86/htmlcxx-0.86.tar.gz -OutFile htmlcxx-0.86.tar.gz"
     tar xvzf htmlcxx-0.86.tar.gz
     del htmlcxx-0.86.tar.gz
     REN htmlcxx-0.86 htmlcxx
